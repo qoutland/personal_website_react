5c762fe25898a9b2067573fc9f644a4fa527b9af
Quin Outland <qoutland@gmail.com>
2019-06-02 20:59:03 -0700

    fixing file structure

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import  'react-router-dom';
 import './App.css';
 
-import NavBar from './components/NavBar';
-import Jumbotron from './components/Jumbotron';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
+import NavBar from './components/NavBar/NavBar';
+import Jumbotron from './components/Jumbotron/Jumbotron';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Thanks from './components/Thanks'
-import Qualifications from './components/Qualifications';
+import Qualifications from './components/Qualifications/Qualifications';
 
 function App() {
   console.log(window.location.pathname)
